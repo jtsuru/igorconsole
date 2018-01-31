fc01a26178e0c8c24d0d77921f433c51273ffb8c
jtsuru <33685861+jtsuru@users.noreply.github.com>
2018-01-31 17:46:33 +0900

    add utils method for tables

--- igorconsole/utils.py
+++ igorconsole/utils.py
@@ -43,6 +43,25 @@ def from_igor_complex_wave_order(array, dtype=np.complex128):
     result.imag = array[:, imag]
     return result
 
+def _str_insert(basestr, index, istr):
+    return basestr[:index] + istr + basestr[index:]
+
+def to_unique_key(strings):
+    result = []
+    apd = result.append
+    for s in strings:
+        s = s.replace("'", "")
+        if s not in result:
+            apd(s)
+        else:
+            i = 0
+            candidate = _str_insert(s, s.rfind("."), "_{}".format(i))
+            while candidate in result:
+                i += 1
+                candidate = _str_insert(s, s.rfind("."), "_{}".format(i))
+            apd(candidate)
+    return result
+
 BOOLS = (bool, np.bool_)
 INT_NUMS = (int, np.bool_, np.integer)
 REAL_NUMS = (int, float, np.bool_, np.integer, np.floating)
